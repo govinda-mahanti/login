5859818e2dd86a0b18fc12787e9207dbd191f622
govinda-mahanti <gmahanti955@gmail.com>
2025-10-24 03:24:24 +0530

    add

---
+++ api/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import router from "../route/userRoutes.js";
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("mongoDB connected successfully."));
+
+const PORT = process.env.PORT || 5000;
+
+app.use("/user", router);
+/* 
+app.use("/register", register);
+app.use("/login", login); */
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
